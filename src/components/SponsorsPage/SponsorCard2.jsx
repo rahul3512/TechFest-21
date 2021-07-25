22dbe56d09327f0b6252d40a398a39f52d6982ea
AryamannSinha <1930031@sliet.ac.in>
2021-07-26 02:42:00 +0530

    changes in sponsorship page completed

--- src/components/SponsorsPage/SponsorCard2.jsx
+++ src/components/SponsorsPage/SponsorCard2.jsx
@@ -3,7 +3,7 @@ function SponsorCard2() {
   return (
     <div>
       <div className="container">
-        <div className="row text-center">
+        <div className="row sponsordesc text-center">
           <h1 className="heading text-center">Partner with us</h1>
           <p className="text">
             orem ipsum dolor sit amet,orem ipsum dolor sit amet,
@@ -12,41 +12,41 @@ function SponsorCard2() {
           </p>
           <div className="col-lg-3 col-sm-12 col-md-12">
             <div className="card card2">
-              <h5 className="card-title">join Our Telegram </h5>
+              <h6 className="card-title">join Our Telegram </h6>
             </div>
           </div>
 
           <div className="col-lg-3 col-sm-12 col-md-12">
             <div className="card card2">
-              <h5 className="card-title">Merchandise Partner</h5>
+              <h6 className="card-title">Merchandise Partner</h6>
             </div>
           </div>
           <div className="col-lg-3 col-sm-12">
             <div className="card card2">
-              <h5 className="card-title">Goodies and Coupon Partner</h5>
+              <h6 className="card-title">Goodies and Coupon</h6>
             </div>
           </div>
           <div className="col-lg-3 col-sm-12">
             <div className="card card2">
-              <h5 className="card-title">Training and Internship</h5>
+              <h6 className="card-title">Training and Internship</h6>
             </div>
           </div>
         </div>
         <div className="row text-center row1">
           <div className="col-lg-3 col-sm-12 left">
             <div className="card card2">
-              <h5 className="card-title">Workshop Partner</h5>
+              <h6 className="card-title">Workshop Partner</h6>
             </div>
           </div>
 
           <div className="col-lg-3 col-sm-12 left">
             <div className="card card2">
-              <h5 className="card-title">Outreach Partner</h5>
+              <h6 className="card-title">Outreach Partner</h6>
             </div>
           </div>
           <div className="col-lg-3 col-sm-12 left">
             <div className="card card2">
-              <h5 className="card-title">Media Partner</h5>
+              <h6 className="card-title">Media Partner</h6>
             </div>
           </div>
         </div>
