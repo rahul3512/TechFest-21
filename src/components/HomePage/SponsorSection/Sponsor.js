2e7a3f964ec8af1fe8b0f4bfb8648ae26641d06c
Ruchika <rruchika349@gmail.com>
2021-08-17 14:20:44 +0530

    sponsor updated

--- src/components/HomePage/SponsorSection/Sponsor.js
+++ src/components/HomePage/SponsorSection/Sponsor.js
@@ -11,7 +11,16 @@ import sponsor4 from '../../../assets/sponsor/sponsor4.jpg';
 import sponsor5 from '../../../assets/sponsor/sponsor5.jpeg';
 import sponsor6 from '../../../assets/sponsor/sponsor6.png';
 import sponsor7 from '../../../assets/sponsor/sponsor7.jpg';
-// import sponsor5 from '../../../assets/images/Rados.png';
+import sponsor8 from '../../../assets/images/sponsor8.jpg';
+import sponsor9 from '../../../assets/images/sponsor9.jpg';
+import sponsor10 from '../../../assets/images/sponsor10.jpg';
+import sponsor11 from '../../../assets/images/sponsor11.jpg';
+import sponsor12 from '../../../assets/images/sponsor12.jpg';
+import sponsor13 from '../../../assets/images/sponsor13.jpg';
+import sponsor14 from '../../../assets/images/sponsor14.jpg';
+import sponsor15 from '../../../assets/images/sponsor15.jpg';
+import sponsor16 from '../../../assets/images/sponsor16.jpg';
+
 
 import { fontSize } from "@material-ui/system";
 
@@ -40,6 +49,17 @@ function HomeSponsor() {
                     <Item><img src={sponsor6} alt='sponsor' className='sponsor-img' /></Item>
                     <Item><img src={sponsor7} alt='sponsor' className='sponsor-img' /></Item>
                     <Item><img src={sponsor} alt='sponsor' className='sponsor-img' /></Item>
+
+                    <Item><img src={sponsor8} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor9} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor10} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor11} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor12} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor13} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor14} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor15} alt='sponsor' className='sponsor-img' /></Item>
+                    <Item><img src={sponsor16} alt='sponsor' className='sponsor-img' /></Item>
+
 
                 </Carousel>
             </div>
